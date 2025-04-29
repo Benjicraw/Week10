a0c8e2acc2a0d1b70d95465a6f642c119c267ef8
Benjicraw <benjicrawford@gmail.com>
2025-04-28 18:53:28 -0600

    week 9

--- eclipse-workspace/MySQL/mysql-java/src/main/resources/projects-schema.sql
+++ eclipse-workspace/MySQL/mysql-java/src/main/resources/projects-schema.sql
@@ -1,34 +1,35 @@
 DROP TABLE IF EXISTS project_category;
-DROP TABLE IF EXISTS step;
 DROP TABLE IF EXISTS material;
-DROP TABLE IF EXISTS project;
+DROP TABLE IF EXISTS step;
 DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS project;
 
-
-CREATE TABLE category (
-    category_id INT /*AUTO_INCREMENT*/ PRIMARY KEY,
-    category_name VARCHAR(128) NOT NULL
-);
 
 CREATE TABLE project (
-    project_id INT /*AUTO_INCREMENT*/ PRIMARY KEY,
+    project_id INT AUTO_INCREMENT PRIMARY KEY
     project_name VARCHAR(128) NOT NULL,
     estimated_hours DECIMAL(7,2),
     actual_hours DECIMAL(7,2),
     difficulty INT,
-    notes TEXT
+    notes TEXT,
+);
+
+CREATE TABLE category (
+    category_id INT NOT NULL AUTO_INCREMENT ,
+    category_name VARCHAR(128) NOT NULL,
+    PRIMARY KEY (category)
 );
 
 CREATE TABLE step (
-    step_id INT /*AUTO_INCREMENT*/ PRIMARY KEY,
+    step_id INT  NOT NULL,
     project_id INT NOT NULL,
     step_text TEXT NOT NULL,
-    step_order INT NOT NULL,
+    step_order INT NOT NULL, 
     FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE
 );
 
 CREATE TABLE material (
-    material_id INT /*AUTO_INCREMENT*/ PRIMARY KEY,
+    material_id INT  NOT NULL,
     project_id INT NOT NULL,
     material_name VARCHAR(128) NOT NULL,
     num_required INT,
@@ -41,5 +42,6 @@ CREATE TABLE project_category (
     category_id INT NOT NULL,
     PRIMARY KEY (project_id, category_id),
     FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE
+    FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE,
+   
 );
